08263024df7ca413139ea663fb9713fe8c48d003
Kacper Korzekwa <kackor11@gmail.com>
2026-03-31 16:16:06 +0200

    zad 7 - done

--- 01_lab/Zadania/07_zad/07_zad.cpp
+++ 01_lab/Zadania/07_zad/07_zad.cpp
@@ -1,1 +1,29 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string napis{};
+
+    cout << "Podaj dowolny napis: ";
+    cin >> napis;
+
+    while (cin.fail()) {
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+
+        cout << "Podano niepoprawne dane..." << endl;
+        cout << "Sprobuj jeszcze raz: ";
+        cin >> napis;
+    }
+
+    int dlugosc_napisu{};
+    dlugosc_napisu = napis.length();
+
+    cout << "Dlugosc napisu " << napis << " = " << dlugosc_napisu;
+
+    return 0;
+}
