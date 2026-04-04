43f7c661967d9f60182aa6eb3548c6e44bd6c5fe
Kacper Korzekwa <kackor11@gmail.com>
2026-04-04 19:10:38 +0200

    vektory cpp zad01 - done

--- 01_lab/Playground/04_test_file.cpp
+++ 01_lab/Playground/04_test_file.cpp
@@ -2,4 +2,13 @@
 #include <string>
 #include <vector>
 
-using namespace std;
+int main() {
+
+    std::vector<int> oceny {1, 2, 3, 4, 5};
+    std::vector<int> punkty_zycia (2, 100);
+
+    std::cout << "Rozmiar vektora oceny: " << oceny.size() << std::endl;
+    std::cout << "Rozmiar vektora punkty zycia: " << punkty_zycia.size() << std::endl;
+
+    return 0;
+}
