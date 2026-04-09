b9a57f36000ecf8bb80109becb30a75c951b00bb
Kacper Korzekwa <kackor11@gmail.com>
2026-04-09 23:30:12 +0200

    zadanie 3 - done

--- projekt_wstepny/03_zad/Binomial_Coefficient/code.cpp
+++ projekt_wstepny/03_zad/Binomial_Coefficient/code.cpp
@@ -1,18 +1,5 @@
 #include <iostream>
 
-int factorial(int x) {
-
-    int res{1};
-
-    for (int i = 1; i <= x; i++)
-    {
-        res *= i;
-    }
-    
-    return res;
-
-}
-
 int binomial(int n, int k) {
 
     if (k == 0) return 1;
@@ -31,8 +18,20 @@ int binomial(int n, int k) {
     return res;
 }
 
-int binomial_recursive() {
-    return 0;
+int binomial_rec_helper(int n, int k, int curr, int acc) {
+    if (curr > k) return acc;
+
+    int next_acc = acc * (n - curr + 1) / curr;
+
+    return binomial_rec_helper(n, k, curr + 1, next_acc);
+}
+
+int binomial_recursive(int n, int k) {
+    if (k == 0) return 1;
+    if (n == 0 || k > n) return 0;
+    if (k > n - k) {k = n - k; }
+
+    return binomial_rec_helper(n, k, 1, 1);
 }
 
 int main() {
--- projekt_wstepny/03_zad/Euclidean_Algorithm/code.cpp
+++ projekt_wstepny/03_zad/Euclidean_Algorithm/code.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
 
+int nwd(int a, int b) {
+    while (b != 0)
+    {
+        int c = a % b;
+        a = b;
+        b = c;
+    }
+    return a;
+}
+
+int nwd_rec(int a, int b) {
+    if (b == 0) {return a; }
+
+    int c = a % b;
+    a = b;
+    b = c;
+
+    return nwd_rec(a, b); 
+}
+
 int main() {
+    int res_ite = nwd(1989, 867);
+    std::cout << "Wynik dla wersji iteracyjnej: " << res_ite << std::endl;
+    int res_rec = nwd_rec(1989, 867);
+    std::cout << "Wynik dla wersji rekurencyjnej: " << res_rec << std::endl;
+
     return 0;
 }
